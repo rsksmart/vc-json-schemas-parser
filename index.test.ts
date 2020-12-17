3c23950b0fccf4b3214832071ec0013cb5570385
Ilan Olkies <ilanolkies@outlook.com>
2020-12-17 13:41:10 -0300

    Add Email credential schema

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,42 @@
 import { decodeJWT } from 'did-jwt'
-import { parseVerifiableCredential } from './'
-import { EmailVerifiableCredential } from './test-cases'
+import { parseVerifiableCredential } from './index'
+import {
+  VerifiableCredentialInvalid,
+  EmailVerifiableCredential,
+  EmailVerifiableCredentialInvalid
+} from './test-cases'
 
 describe('parser', () => {
-  test('errors on invalid credential', () => {
+  test('errors on invalid credential schema', () => {
+    expect.assertions(1)
     const jwt = decodeJWT(EmailVerifiableCredential)
     expect(() => parseVerifiableCredential('INVALID', jwt.payload)).toThrowError('Invalid schema')
+  })
+
+  test('errors on invalid credential', () => {
+    expect.assertions(1)
+    const jwt = decodeJWT(VerifiableCredentialInvalid)
+    expect(() => parseVerifiableCredential('Email', jwt.payload)).toThrowError('Invalid credential')
+  })
+
+  describe('Email Verifiable Credential', () => {
+    test('errors on invalid Email credential', () => {
+      expect.assertions(1)
+      const jwt = decodeJWT(EmailVerifiableCredentialInvalid)
+      expect(() => parseVerifiableCredential('Email', jwt.payload)).toThrowError('Invalid Email credential')
+    })
+
+    test('parses Email credential', () => {
+      expect.assertions(1)
+      const jwt = decodeJWT(EmailVerifiableCredential)
+      expect(parseVerifiableCredential('Email', jwt.payload)).toEqual({
+        'Email': {
+          text: 'ilan@iovlabs.org',
+          prefix: {
+            en: 'Email address'
+          }
+        }
+      })
+    })
   })
 })
--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
 import { JWTPayload } from 'did-jwt'
 
-function parseVerifiableCredential(schema: string, jwtDecoded: JWTPayload) {
+function parseVerifiableCredential(schema: string, payload: JWTPayload) {
+  if (!payload.credentialSubject) throw new Error('Invalid credential')
+  const { credentialSubject } = payload
   switch(schema) {
+    case 'Email': {
+      if (!credentialSubject.emailAddress) throw new Error('Invalid Email credential')
+      return {
+        'Email': {
+          text: credentialSubject.emailAddress,
+          prefix: {
+            en: 'Email address'
+          }
+        }
+      }
+    }
     default: throw new Error('Invalid schema')
   }
 }
